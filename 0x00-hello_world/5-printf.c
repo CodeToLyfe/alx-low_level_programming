b8f49b126a0957294c461d17af2dc4d5d6917f11
Bruce Atsem <bruceatsem@gmail.com>
2023-07-13 18:54:53 -0700

    Print in c using prinf fn

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry poine
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,\n");
+	return (0);
+}
+
