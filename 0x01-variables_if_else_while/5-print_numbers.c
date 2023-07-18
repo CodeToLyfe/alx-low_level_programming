debf0aa5e0086d63536fcf3733513028a4824763
Bruce Atsem <bruceatsem@gmail.com>
2023-07-17 19:54:04 -0700

    Print numbers 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description - Print digits 0 - 9
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num  = 0; num <= 9; num++)
+	{
+		printf("%d",num );
+	}
+	printf('\n');
+	return (0);
+
+}
