636d34e57e3866ca7320551da1855597035096bd
Bruce Atsem <bruceatsem@gmail.com>
2023-07-18 10:46:53 -0700

    Hot fix print combos of 2 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 38; num1 <= 46; num1++)
+	for (num1 = 48; num1 <= 56; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 47; num2++)
+		for (num2 = num1 + 1; num2 <= 57; num2++)
 		{
 			putchar((char)num1);
 			putchar((char)num2);
-			if (num1 != 46)
+			if (num1 != 56)
 			{
 				putchar(',');
 				putchar(' ');
